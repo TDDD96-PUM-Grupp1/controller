f44a2b3f558761118a9569fe355a8a2de905d4b3
Alexander Wilkens <alewi684@student.liu.se>
2018-03-23 09:57:15 +0100

    Clean up app.js render method

--- src/App.js
+++ src/App.js
@@ -39,53 +39,64 @@ class App extends React.Component {
     console.log('Toggling Window to main');
   }
 
+  renderDefault() {
+    return (
+      <WelcomeButton
+        className="WelcomeButton"
+        requestInstances={this.com.requestInstances}
+        enterMainWindow={this.enterMainWindow}
+      />
+    );
+  }
+
+  renderSessionList() {
+    return (
+      <div>
+        <FilterSession />
+        <SessionList activeSessions={this.instances} enterSessionWindow={this.enterSessionWindow} />
+        <SensorOutput />
+      </div>
+    );
+  }
+
+  renderSession() {
+    return (
+      <div>
+        <UsernameInput instanceName={this.instanceName} onInputSubmit={this.com.joinInstance} />
+        <button className="Random">Random</button>
+        <button className="Join">Join</button>
+        {this.state.connectionActive ? (
+          <div>
+            <SensorOutput onSensorChange={this.com.updateSensorData} />
+          </div>
+        ) : (
+          <div>
+            <SensorOutput />
+          </div>
+        )}
+      </div>
+    );
+  }
+
   render() {
-    // The greeting screen
+    let stateRender;
+
     if (this.state.windowState === 'default') {
-      return (
-        <div className="App">
-          <WelcomeScreen />
-          <WelcomeButton
-            className="WelcomeButton"
-            requestInstances={this.com.requestInstances}
-            enterMainWindow={this.enterMainWindow}
-          />
-        </div>
-      );
-      // The screen showing all possible sessions to join
+      stateRender = this.renderDefault();
     } else if (this.state.windowState === 'sessionList') {
-      return (
-        <div className="App">
-          <WelcomeScreen />
-          <FilterSession />
-          <SessionList
-            activeSessions={this.instances}
-            enterSessionWindow={this.enterSessionWindow}
-          />
-          <SensorOutput />
-        </div>
-      );
-      // The screen showing detailed information of a single session
+      stateRender = this.renderSessionList();
     } else if (this.state.windowState === 'session') {
-      return (
-        <div className="App">
-          <WelcomeScreen />
-          <UsernameInput instanceName={this.instanceName} onInputSubmit={this.com.joinInstance} />
-          <button className="Random">Random</button>
-          <button className="Join">Join</button>
-          {this.state.connectionActive ? (
-            <div>
-              <SensorOutput onSensorChange={this.com.updateSensorData} />
-            </div>
-          ) : (
-            <div>
-              <SensorOutput />
-            </div>
-          )}
-        </div>
-      );
+      stateRender = this.renderSession();
+    } else {
+      return <div className="App">no state is selected to show!</div>;
     }
-    return <div className="App">no state is selected to show!</div>;
+
+    return (
+      <div className="App">
+        <WelcomeScreen />
+        {stateRender}
+      </div>
+    );
   }
 }
 
