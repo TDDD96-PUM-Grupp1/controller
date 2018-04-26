c5647d38f46ef2a6846d7cd8eae2410551c17195
joelnir <joel.oscarsson@hotmail.com>
2018-04-26 11:54:15 +0200

    Add xonfig to ignore rule no-mixed-operators

--- src/components/NameRandomizer.js
+++ src/components/NameRandomizer.js
@@ -214,7 +214,6 @@ class NameRandomizer {
    * credit to Francisc from Stackoverflow for this one
    */
   static randomIntFromInterval(min, max) {
-    // eslint-disable-next-line
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 }
