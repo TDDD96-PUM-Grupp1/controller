3700945a4f42ddd8ce8d2efd744cfa2cbe720a89
Tim Håkansson <34460034+Thraix@users.noreply.github.com>
2018-05-02 10:29:44 +0200

    Fix the controller having weird behaviour at 90 degrees angles. (#61)
    * Fix sensor having weird behaviour over 90 degrees in gamma direction
    
    * Fix bug and make SensorOutput use SensorManager
    
    * Move comment

--- src/App.js
+++ src/App.js
@@ -152,7 +152,6 @@ class App extends Component {
 
   render() {
     let stateRender;
-
     if (this.state.windowState === 'splashScreen') {
       stateRender = this.renderSplashScreen();
     } else if (this.state.windowState === 'instancePicker') {
--- src/components/SensorOutput.js
+++ src/components/SensorOutput.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropType from 'prop-types';
+import SensorManager from '../SensorManager';
 
 class SensorOutput extends Component {
   constructor(props) {
@@ -8,61 +8,42 @@ class SensorOutput extends Component {
     this.state = {
       beta: 0,
       gamma: 0,
-      betaBase: 0,
-      gammaBase: 0,
     };
 
     // Make event-based callbacks bind correctly
-    this.handleCalibrationClick = this.handleCalibrationClick.bind(this);
     this.handleDeviceOrientation = this.handleDeviceOrientation.bind(this);
+
+    this.sensorManager = new SensorManager(this.handleDeviceOrientation);
   }
 
   componentDidMount() {
-    // Event listener for device orientation
-    window.addEventListener('deviceorientation', this.handleDeviceOrientation);
+    this.sensorManager.bindEventListener();
   }
 
   componentWillUnmount() {
-    // Make sure to unbind the event listener when component unmounts
-    window.removeEventListener('deviceorientation', this.handleDeviceOrientation);
+    this.sensorManager.unbindEventListener();
   }
 
   // Sets state to device orientation
-  handleDeviceOrientation(event) {
+  handleDeviceOrientation(beta, gamma) {
     this.setState({
-      beta: event.beta - this.state.betaBase || 'N/A',
-      gamma: event.gamma - this.state.gammaBase || 'N/A',
-    });
-    this.props.onSensorChange(this.state.beta, this.state.gamma);
-  }
-
-  // Sets a new baseline for sensors
-  handleCalibrationClick() {
-    this.setState({
-      betaBase: this.state.beta + this.state.betaBase,
-      gammaBase: this.state.gamma + this.state.gammaBase,
+      beta,
+      gamma,
     });
   }
 
   render() {
+    const { betaBase, gammaBase } = this.sensorManager.getCalibratedValues();
     return (
       <div>
         <div>Beta: {Math.round(this.state.beta)}</div>
         <div>Gamma: {Math.round(this.state.gamma)}</div>
-        <div>BetaBase: {Math.round(this.state.betaBase)}</div>
-        <div>GammaBase: {Math.round(this.state.gammaBase)}</div>
-        <button onClick={this.handleCalibrationClick}>Calibrate</button>
+        <div>BetaBase: {Math.round(betaBase)}</div>
+        <div>GammaBase: {Math.round(gammaBase)}</div>
+        <button onClick={this.sensorManager.calibrate}>Calibrate</button>
       </div>
     );
   }
 }
-
-SensorOutput.defaultProps = {
-  onSensorChange: () => {},
-};
-
-SensorOutput.propTypes = {
-  onSensorChange: PropType.func,
-};
 
 export default SensorOutput;
