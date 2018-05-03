3cabf7f07fae13107b7053b8784a9341b23c8cc4
Tim Håkansson <34460034+Thraix@users.noreply.github.com>
2018-05-03 21:39:17 +0200

    Change deployed deepstream ip (#65)
    * Change deployed ds ip
    
    * Fix lint
    
    * Remove ds server from config and change client name to gameclient
    
    * Change config id to gameclient
    
    * Change auth id

--- src/config.js
+++ src/config.js
@@ -4,10 +4,9 @@ Configuration file for different project parameters.
 
 const settings = {
   communication: {
-    host_ip: 'ds.tddd96.i4demo.com:80',
-    service_name: 'tddd96client',
+    service_name: 'gameclient',
     auth: {
-      id: 'tddd96client',
+      id: 'gameclient',
       password: '6da9475cfe8525232dfb42b293f56acea8333e4ee7ea56d961d9dbfe4d2ea324',
     },
     tickrate: 30,
