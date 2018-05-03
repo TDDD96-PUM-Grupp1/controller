4e0b37395dd30aa8883f57f8665bc2aea61e460d
Tim Hakansson <timha404@liu.se>
2018-05-03 21:37:15 +0200

    Change auth id

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ const settings = {
   communication: {
     service_name: 'gameclient',
     auth: {
-      id: 'client',
+      id: 'gameclient',
       password: '6da9475cfe8525232dfb42b293f56acea8333e4ee7ea56d961d9dbfe4d2ea324',
     },
     tickrate: 30,
