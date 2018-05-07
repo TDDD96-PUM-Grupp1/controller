03b63befa2ebcb77041d39a9d5ced9435b830532
Tim Håkansson <34460034+Thraix@users.noreply.github.com>
2018-05-07 18:36:17 +0200

    Fix service name (#66)

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ Configuration file for different project parameters.
 
 const settings = {
   communication: {
-    service_name: 'gameclient',
+    service_name: 'game',
     auth: {
       id: 'gameclient',
       password: '6da9475cfe8525232dfb42b293f56acea8333e4ee7ea56d961d9dbfe4d2ea324',
